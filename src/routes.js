0182dc09198db3c2f205579e3658cd716078477c
Ilia <ilia@bookjane.com>
2019-09-14 12:50:49 -0400

    init setup

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import styled from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Pages
+import HomePage from './pages/Home';
+// import Register from './pages/Auth/Register';
+// import Verify from './pages/Auth/Verify';
+// import ForgotPassword from './pages/Auth/ForgotPassword';
+// import ResetPassword from './pages/Auth/ResetPassword';
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Routes = () => (
+  <Router>
+    <Wrapper>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={HomePage}
+        />
+
+        {/* <Route
+          exact
+          path="/register"
+          component={Register}
+        />
+
+        <Route
+          exact
+          path="/verify"
+          component={Verify}
+        />
+
+        <Route
+          exact
+          path="/forgot-password"
+          component={ForgotPassword}
+        />
+
+        <Route
+          exact
+          path="/reset-password"
+          component={ResetPassword}
+        /> */}
+      </Switch>
+      <ToastContainer
+        autoClose={3500}
+      />
+    </Wrapper>
+  </Router>
+);
+
+export default Routes;
